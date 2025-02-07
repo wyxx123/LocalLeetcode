471dd45dc0032469d6a4984a2da74f0b56cd3bfd
yuidaisuki <221250122@smail.nju.edu.cn>
2025-02-07 23:59:11 +0800

    add clean

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 @echo off
 if not exist build mkdir build
+conan install . --output-folder=build --build=missing
 cd build
 
 cmake .. -G "MinGW Makefiles" -DCMAKE_TOOLCHAIN_FILE="conan_toolchain.cmake" -DCMAKE_BUILD_TYPE=Release
