55a9fb01be064e56b3e50bb51db29923a5b9a547
yuidaisuki <221250122@smail.nju.edu.cn>
2025-02-08 17:47:47 +0800

    对于2_list的测试用例进行了更新, 但是报错原因尚未知晓

--- problems/1_array/1_binary_search.cpp
+++ problems/1_array/1_binary_search.cpp
@@ -1,22 +1,5 @@
 #include "base.hpp"
-
-// 左闭右闭写法
-//  int search(vector<int>& nums, int target) {
-//    int middle;
-//    int left = 0, right = nums.size() - 1;
-// 循环条件的判断就是看条件是否符合区间定义
-//   while (left <= right) {
-//     middle = left + (right - left) / 2;
-//     if (nums[middle] > target) {
-//       right = middle - 1;
-//     } else if (nums[middle] < target) {
-//       left = middle + 1;
-//     } else {
-//       return middle;
-//     }
-//   }
-//   return -1;
-// }
+#include "test.hpp"
 
 // 左闭右开写法
 int search(std::vector<int> &nums, int target) {
@@ -36,36 +19,9 @@ int search(std::vector<int> &nums, int target) {
   return -1;
 }
 
-struct TestCase {
-  std::vector<int> nums;
-  int target;
-  int expected_index;
-};
 
-class Tester : public testing::TestWithParam<TestCase> {};
+LEETCODE_TEST(search, -1, (std::vector<int>{}), 5)
+LEETCODE_TEST(search, 0, (std::vector<int>{1}), 1)
+LEETCODE_TEST(search, 0, (std::vector<int>{1, 2, 3, 4, 5}), 1)
+LEETCODE_TEST(search, 2, (std::vector<int>{1, 2, 3, 4, 5}), 3)
 
-TEST_P(Tester, FindsTargetIndex) {
-  // using ::operator<<;
-  TestCase test = GetParam();
-  std::vector<int> nums = test.nums;
-  int result = search(nums, test.target);
-  EXPECT_EQ(result, test.expected_index)
-      << "Failed for input: " << nums << " with target: " << test.target;
-}
-
-INSTANTIATE_TEST_SUITE_P(
-    VariousInputs, Tester,
-    testing::Values(
-        TestCase{{}, 5, -1}, TestCase{{1}, 1, 0}, TestCase{{1}, 2, -1},
-        TestCase{{1, 2, 3, 4, 5}, 1, 0}, TestCase{{1, 2, 3, 4, 5}, 3, 2},
-        TestCase{{1, 2, 3, 4, 5}, 5, 4}, TestCase{{1, 2, 3, 4, 5}, 0, -1},
-        TestCase{{1, 3, 5, 7, 9}, 4, -1}, TestCase{{1, 2, 3, 4, 5}, 6, -1},
-        // TestCase{ {1, 2, 2, 2, 3}, 2, 1 }, //
-        // 二分查找对存在重复元素的数组不适用
-        TestCase{{1, 2, 2, 2, 3}, 4, -1}, TestCase{{1, 2, 3, 4}, 3, 2},
-        TestCase{{1, 2, 3, 4, 5}, 3, 2}));
-
-int main(int argc, char **argv) {
-  testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
--- problems/2_list/1_remove_linked_list_elements.cpp
+++ problems/2_list/1_remove_linked_list_elements.cpp
@@ -1,56 +1,44 @@
 #include "base.hpp"
+#include "test.hpp"
+#include <vector>
 
+// 错误示例
 ListNode *removeElements(ListNode *head, int val) {
   ListNode *dummy = new ListNode(0, head);
+  return dummy;
 }
 
-struct TestCase {
-  std::vector<int> input;
-  int val;
-  std::vector<int> expected;
-};
-
-std::ostream &operator<<(std::ostream &os, const struct TestCase &test) {
-  os << "{ nums: " << listToVector(vectorToList(test.input))
-     << ", val: " << test.val << ", expected: " << test.expected << " }";
-  return os;
-}
-
-class Tester : public ::testing::TestWithParam<TestCase> {};
-
-TEST_P(Tester, RemovesSpecifiedElements) {
-  TestCase test = GetParam();
-  ListNode *head = vectorToList(test.input);
-  ListNode *result = removeElements(head, test.val);
-  std::vector<int> result_vec = listToVector(result);
-  EXPECT_EQ(result_vec, test.expected)
-      << "Failed for input: " << test.input << " with val: " << test.val;
-
+bool cmp(ListNode* result, std::vector<int> expected) {
+  int res_size = 0;
   while (result != nullptr) {
-    ListNode *temp = result;
+    if (res_size < expected.size() && result->val != expected[res_size])
+      return false;
+    ++res_size;
+    ListNode* tmp = result;
     result = result->next;
-    delete temp;
+    delete tmp;
   }
+  if (res_size != expected.size()) return false;
+  return true;
 }
 
-INSTANTIATE_TEST_SUITE_P(
-    VariousInputs, Tester,
-    testing::Values(TestCase{{}, 3, {}}, TestCase{{3, 3, 3}, 3, {}},
-                    TestCase{{1, 2, 4, 5}, 3, {1, 2, 4, 5}},
-                    TestCase{{3, 3, 1, 2, 4}, 3, {1, 2, 4}},
-                    TestCase{{1, 3, 3, 3, 4, 5}, 3, {1, 4, 5}},
-                    TestCase{{1, 2, 3, 3, 3}, 3, {1, 2}}, TestCase{{3}, 3, {}},
-                    TestCase{{2}, 3, {2}},
-                    TestCase{{1, 2, 2, 3, 3, 4}, 2, {1, 3, 3, 4}},
-                    TestCase{{2, 2, 2, 2}, 2, {}},
-                    TestCase{{1, 2, 2, 4, 5, 5}, 3, {1, 2, 2, 4, 5, 5}},
-                    TestCase{{-1, -1, 0, 1, 2}, -1, {0, 1, 2}},
-                    TestCase{{0, 1, 2, 0, 3, 0, 4}, 0, {1, 2, 3, 4}},
-                    TestCase{{-3, -2, -2, 0, 1, 2, -2}, -2, {-3, 0, 1, 2}},
-                    TestCase{{}, 0, {}},
-                    TestCase{{1, 2, 3, 4, 5}, 6, {1, 2, 3, 4, 5}}));
+LEETCODE_TEST_EX(removeElements, (std::vector<int>{}), cmp, vectorToList(std::vector<int>{}), 3)
+LEETCODE_TEST_EX(removeElements, (std::vector<int>{}), cmp, vectorToList(std::vector<int>{}), 3)
 
-int main(int argc, char **argv) {
-  testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
+// INSTANTIATE_TEST_SUITE_P(
+//     VariousInputs, Tester,
+//     testing::Values(TestCase{{}, 3, {}}, TestCase{{3, 3, 3}, 3, {}},
+//                     TestCase{{1, 2, 4, 5}, 3, {1, 2, 4, 5}},
+//                     TestCase{{3, 3, 1, 2, 4}, 3, {1, 2, 4}},
+//                     TestCase{{1, 3, 3, 3, 4, 5}, 3, {1, 4, 5}},
+//                     TestCase{{1, 2, 3, 3, 3}, 3, {1, 2}}, TestCase{{3}, 3, {}},
+//                     TestCase{{2}, 3, {2}},
+//                     TestCase{{1, 2, 2, 3, 3, 4}, 2, {1, 3, 3, 4}},
+//                     TestCase{{2, 2, 2, 2}, 2, {}},
+//                     TestCase{{1, 2, 2, 4, 5, 5}, 3, {1, 2, 2, 4, 5, 5}},
+//                     TestCase{{-1, -1, 0, 1, 2}, -1, {0, 1, 2}},
+//                     TestCase{{0, 1, 2, 0, 3, 0, 4}, 0, {1, 2, 3, 4}},
+//                     TestCase{{-3, -2, -2, 0, 1, 2, -2}, -2, {-3, 0, 1, 2}},
+//                     TestCase{{}, 0, {}},
+//                     TestCase{{1, 2, 3, 4, 5}, 6, {1, 2, 3, 4, 5}}));
+
